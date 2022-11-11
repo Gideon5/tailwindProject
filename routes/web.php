ce298272e72b008eaa5b528c98a6c31a764ca627
elgrande <abaaneg@gmail.com>
2022-11-11 17:32:29 +0000

    registration page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('authentication.login');
 });
+
+Route::get('/registration', function () {
+    return view('authentication.registration');
+});
